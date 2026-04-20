63d812ca8edaadcabc94bc80fba2a7b6c1a72c52
Keinamvy <nguyennhatminhlol90@gmail.com>
2026-04-20 06:16:37 +0000

    Susfs 2.1.0 inline hook
    Source: https://github.com/JackA1ltman/NonGKI_Kernel_Build_2nd

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -7,6 +7,10 @@
 #include <asm/setup.h>
 #endif
 
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+#include <linux/susfs.h>
+#endif
+
 #ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 #define INITRAMFS_STR_FIND "skip_initramf"
 #define INITRAMFS_STR_REPLACE "want_initramf"
@@ -27,8 +31,20 @@ static void proc_command_line_init(void) {
 }
 #endif
 
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+extern struct static_key_true susfs_set_fake_cmdline_or_bootconfig_key_true;
+extern void susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
+#endif
+
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+	if (static_branch_likely(&susfs_set_fake_cmdline_or_bootconfig_key_true)) {
+		susfs_spoof_cmdline_or_bootconfig(m);
+		seq_putc(m, '\n');
+		return 0;
+	}
+#endif
 #ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 	seq_printf(m, "%s\n", proc_command_line);
 #else
